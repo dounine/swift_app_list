37041163b295c597dde85a86ba3480ca1ba60bed
lake <102535481@qq.com>
2024-08-12 12:11:13 +0800

    修复显示应用不全bug
    由于获取的itemName为空导致的

--- swift_app_list/LSApp.swift
+++ swift_app_list/LSApp.swift
@@ -12,12 +12,14 @@ import UIKit
 @objc protocol LSApplicationWorkspace {
     static func defaultWorkspace() -> Self
     func allInstalledApplications() -> [NSObject]
+    func allApplications() -> [NSObject]
     func installedPlugins() -> [NSObject]
     func openApplicationWithBundleID(_ bundleID: AnyObject!) -> Bool
 }
 
 @objc protocol LSAppProxy {
     var itemName: NSString { get }
+    var localizedName: NSString { get }
     var containingBundle: NSObject { get }
     var bundleIdentifier: NSString { get }
     var shortVersionString: NSString { get }
@@ -42,11 +44,14 @@ struct LSApp {
         }
         var list = [Info]()
         for item in set.values {
-            guard let appName = item.perform(#selector(getter: LSAppProxy.itemName)) else { continue }
+            var appName = item.perform(#selector(getter: LSAppProxy.itemName))
+            if appName == nil {
+                appName = item.perform(#selector(getter: LSAppProxy.localizedName))
+            }
             guard let bundleId = item.perform(#selector(getter: LSAppProxy.bundleIdentifier)) else { continue }
             guard let version = item.perform(#selector(getter: LSAppProxy.shortVersionString)) else { continue }
             let icon = UIImage._applicationIconImage(forBundleIdentifier: bundleId.takeUnretainedValue() as? String, format: 1, scale: UIScreen.main.scale)
-            list.append(Info(appName: appName.takeUnretainedValue() as! String, version: version.takeUnretainedValue() as! String, bundleId: bundleId.takeUnretainedValue() as! String, icon: icon as? UIImage))
+            list.append(Info(appName: appName!.takeUnretainedValue() as! String, version: version.takeUnretainedValue() as! String, bundleId: bundleId.takeUnretainedValue() as! String, icon: icon as? UIImage))
         }
         return list
     }
